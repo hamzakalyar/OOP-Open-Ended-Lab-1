b1143a3f0350a3e499ad3e0aa887a1b0a17b59ca
jahanzaibaamir57 <146338026+jahanzaibaamir57@users.noreply.github.com>
2024-03-21 14:36:53 +0500

    Update Student.h

--- Student.h
+++ Student.h
@@ -1,23 +1,50 @@
 #pragma once
 #include<iostream>
 #include<string>
-#include <vector>
+#include <list>
+#include"Course.h"
 
 using namespace std;
+
 
 class Student {
 private:
     int studentID;
     string name;
     string email;
-    vector<string> coursesEnrolled;
+    list<Course*> coursesEnrolled;
 
 public:
-    Student(int id, const string& studentName, const string& emailAddress);
+    // Constructor
+    Student(int id, const string& studentName, const string& emailAddress) : studentID(id), name(studentName), email(emailAddress) {}
 
-    void enrollCourse(const string& course);
-    void dropCourse(const string& course);
-    void viewCourses() const;
+
+    /*void getName(string StudentName) {
+        name = StudentName;
+    }*/
+
+    // Method to enroll in a course
+    void enrollCourse(Course* course) {
+        coursesEnrolled.push_back(course);
+        cout << name << " enrolled in " << course->getCourseName() << "." << endl;
+    }
+
+    // Method to drop a course
+    void dropCourse(Course* course) {
+        coursesEnrolled.remove(course);
+        cout << name << " dropped " << course->getCourseName() << "." << endl;
+    }
+
+    // Method to view enrolled courses
+    void viewCourses() const {
+        cout << name << " is enrolled in the following courses:" << endl;
+        for (Course* course : coursesEnrolled) {
+            cout << course->getCourseName() << endl;
+        }
+    }
+
+    //For geting names of Students
+    string Name() {
+        return name;
+    }
 };
-
-
