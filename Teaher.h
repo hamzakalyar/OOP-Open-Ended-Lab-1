f6079ae13ecd3da4c507f09e5d34a04c1b35ca9d
hamzakalyar <146337680+hamzakalyar@users.noreply.github.com>
2024-03-21 12:04:56 +0500

    Update Teaher.h

--- Teaher.h
+++ Teaher.h
@@ -1,27 +1,50 @@
 #pragma once
 #include <iostream>
-#include<string>
+#include <string>
+#include <list>
+#include "Course.h" 
 
 using namespace std;
 
-class Course;
+class Course; 
 
+class Teacher {
+private:
+    string teacherId;
+    string email;
+    list<Course*> courses; 
 
-class Teacher
-{
-	string studentId;
-	string Email;
 public:
-	string name;
+    string name;
 
-	void getId(string Id) {
-		studentId = Id;
-	}
+    
+    void setId(const string& id) {
+        teacherId = id;
+    }
 
-	void getEmail(string email) {
-		Email = email;
-	}
+    void setEmail(const string& emailAddress) {
+        email = emailAddress;
+    }
 
+   
+    void assignCourse(Course* course) {
+        courses.push_back(course);
+    }
 
+    
+    void removeCourse(Course* course) {
+        courses.remove(course);
+    }
+
+    
+    void viewCourses() const {
+        cout << "Courses taught by " << name << ":" << endl;
+        for (const auto& course : courses) {
+            cout << course->getCourseName() << endl;
+        }
+    }
 };
+
+
+
 
